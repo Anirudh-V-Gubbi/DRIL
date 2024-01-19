cd026623014076ebe68e789e4879af4f6030b91d
Nanda Kishor Vinod <nandakishorvinod2020@gmail.com>
2024-01-19 17:41:21 +0530

    Removed dril_cp as a macro

--- lib/macros.h
+++ lib/macros.h
@@ -4,28 +4,26 @@
 #define OS_HANDLE HMODULE
 #define fptr FARPROC
 #define lib_find(handle,symbol) GetProcAddress(handle,symbol)
-#define drilcp while(!copyFile(GetPath(),CopyPath())){ Sleep(10);}
+
 #define dril_close(handle) FreeLibrary(handle)
 #define dril_load(handle) LoadLibraryA(handle)
-#define dril_reload() Load(0)
+#define dril_reload() Load(1)
 #elif __linux__
 #include <dlfcn.h>
 #define OS_HANDLE void*
 #define fptr void*
 #define lib_find((handle,symbol) dlsym((handle,symbol)
-#define drilcp
 #define dril_close(handle) dlclose(handle)
 #define dril_load(handle) dlopen(handle)
-#define dril_reload() Load(1)
+#define dril_reload() Load(0)
 #elif __APPLE__
 #include <dlfcn.h>
 #define OS_HANDLE void*
 #define fptr void*
 #define lib_find((handle,symbol) dlsym((handle,symbol)
-#define drilcp
 #define dril_close(handle) dlclose(handle)
 #define dril_load(handle) dlopen(handle)
-#define dril_reload() Load(1)
+#define dril_reload() Load(0)
 #endif
 
 
