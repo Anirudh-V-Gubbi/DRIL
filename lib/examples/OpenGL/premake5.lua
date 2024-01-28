ad5ef4aad38e5c8b74dd53f083f7413c53d70058
vxshxk <vishakkamath.221ec226@nitk.edu.in>
2024-01-28 19:06:23 +0530

    Added OpenGL lib example

---
+++ lib/examples/OpenGL/premake5.lua
@@ -0,0 +1,53 @@
+workspace "OpenGL"
+  configurations {"Debug", "Release"}
+  language "C++"
+  targetdir "bin/%{cfg.buildcfg}"
+  location "build"
+  outputdir= "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+  include "dep/GLFW"
+
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "RELEASE" }
+    optimize "On"
+  
+  project "OpenGLlib"
+    kind "SharedLib"
+    location "build"
+    files
+    {
+      "glad.c",
+      "dep/stb_image/stb_image.cpp",
+      "dep/stb_image/stb_image.h",
+      "dep/glm/**.h",
+      "dep/glm/**.inl"
+    }
+    includedirs
+    {
+      "dep/GLFW",
+      "dep/glad",
+      "dep/glm",
+      "dep/stb_image",
+      "dep/include"
+    }
+    links
+    {
+      "GLFW"
+    }
+    
+
+  project "OpenGLRuntime"
+    kind "ConsoleApp"
+    location "build"
+    files { "main.cpp"}
+    links { 
+      "OpenGLlib",
+      "GLFW",
+      "opengl32",
+      "shell32", 
+      "gdi32" 
+    }
+    includedirs { "dep/include/"}
