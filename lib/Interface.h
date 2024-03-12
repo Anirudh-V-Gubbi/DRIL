bc9898698430292aadc5aa0a2b1eb425addcd5c9
Nanda Kishor Vinod <nandakishorvinod2020@gmail.com>
2024-03-13 00:26:24 +0530

    Windows Parser Integration-I

--- lib/Interface.h
+++ lib/Interface.h
@@ -14,7 +14,8 @@
 //                           DRIL
 // #############################################################################
 
-class DRIL {
+class DRIL
+{
 public:
   DRIL(std::string path) : m_path(path), parser(path) {}
 
@@ -22,23 +23,32 @@ public:
   void ReloadLibrary() { Reload(); }
 
   template <typename Ret, typename... Args>
-  Ret Execute(const char *name, Args... args) {
+  Ret Execute(const char *name, Args... args)
+  {
     // Lookup the function address
     auto symbol = m_symbols.find(name);
-    if (symbol != m_symbols.end()) {
+    if (symbol != m_symbols.end())
+    {
       // Cast the address to the appropriate function type and call it,
       // forwarding all arguments
       return reinterpret_cast<Ret (*)(Args...)>(symbol->second.memory_adress)(args...);
-    } else {
+    }
+    else
+    {
       throw std::runtime_error(std::string("Function not found: ") + name);
     }
   }
 
-  template <typename T> T *GetVar(const char *name) {
+  template <typename T>
+  T *GetVar(const char *name)
+  {
     auto symbol = m_symbols.find(name);
-    if (symbol != m_symbols.end()) {
+    if (symbol != m_symbols.end())
+    {
       return reinterpret_cast<T *>(symbol->second.memory_adress);
-    } else {
+    }
+    else
+    {
       // We didn't find the variable. Throws an error
       throw std::runtime_error(std::string("Variable not found") + name);
     }
@@ -57,17 +67,20 @@ private:
   void *m_libHandle;
   std::unordered_map<std::string, SymbolInfo> m_symbols;
 
-  bool copyFile(const char *sourcePath, const char *destinationPath) {
+  bool copyFile(const char *sourcePath, const char *destinationPath)
+  {
     // Open source file for reading in binary mode
     std::ifstream sourceFile(sourcePath, std::ios::binary);
-    if (!sourceFile) {
+    if (!sourceFile)
+    {
       std::cerr << "Error opening source file: " << sourcePath << std::endl;
       return false;
     }
 
     // Open destination file for writing in binary mode
     std::ofstream destinationFile(destinationPath, std::ios::binary);
-    if (!destinationFile) {
+    if (!destinationFile)
+    {
       std::cerr << "Error opening destination file: " << destinationPath
                 << std::endl;
       return false;
@@ -76,7 +89,8 @@ private:
     // Copy content from source to destination
     destinationFile << sourceFile.rdbuf();
 
-    if (destinationFile.fail()) {
+    if (destinationFile.fail())
+    {
       std::cerr << "Error copying file." << std::endl;
       return false;
     }
@@ -86,45 +100,42 @@ private:
     return true;
   }
 
-  void Load(bool isWindowsReload) {
-    if (isWindowsReload == 0) {
-      m_libHandle = dril_load(m_path.c_str());
-    } else {
-      std::string path = m_path;
-      size_t pos = path.rfind('.'); // To find the last .
-      if (pos != std::string::npos) {
+  void Load(bool isWindowsReload)
+  {
+    if (isWindowsReload == 0)
+    {
+      m_libHandle = dril_load(GetPath());
+    }
+    else
+    {
+      std::string path = GetPath();
+      size_t pos = path.rfind('.');
+      if (pos != std::string::npos)
+      {
         path.insert(pos, "_Copy");
       }
       const char *CopyPath = path.c_str();
+      copyFile(GetPath(), CopyPath);
       m_libHandle = dril_load(CopyPath);
     }
     LoadSymbols();
   }
 
-  void Reload() {
+  void Reload()
+  {
     OS_HANDLE handle = static_cast<OS_HANDLE>(m_libHandle);
     dril_close(handle);
     m_symbols.clear();
-
-#ifdef _WIN32
-    std::string path = m_path;
-    size_t pos = path.rfind('.');
-    if (pos != std::string::npos) {
-      path.insert(pos, "_Copy");
-    }
-    const char *CopyPath = path.c_str();
-
-    copyFile(path.c_str(), CopyPath);
-#endif
-
     dril_reload();
   }
 
-  void LoadSymbols() {
+  void LoadSymbols()
+  {
     m_symbols = parser.ExtPrsSymbolTable();
 
     OS_HANDLE handle = static_cast<OS_HANDLE>(m_libHandle);
-    for (auto &symbol : m_symbols) {
+    for (auto &symbol : m_symbols)
+    {
       m_symbols[symbol.first].memory_adress =
           lib_find(handle, symbol.first.c_str());
     }
--- lib/macros.h
+++ lib/macros.h
@@ -2,6 +2,7 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#include <fstream>
 #define OS_HANDLE HMODULE
 #define FPTR FARPROC
 #define lib_find(handle, symbol) GetProcAddress(handle, symbol)
@@ -9,6 +10,8 @@
 #define dril_close(handle) FreeLibrary(handle)
 #define dril_load(handle) LoadLibraryA(handle)
 #define dril_reload() Load(1)
+#define popen _popen
+#define pcose _pclose
 
 #elif __linux__
 #include <dlfcn.h>
